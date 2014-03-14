ddc0836c7640631a08abe7176c6bca6a119f7f9a
Kyle Chadha <kyle.chadha@gmail.com>
2014-03-13 18:36:58 -0700

    Finalize restful node app

---
+++ public/javascripts/global.js
@@ -0,0 +1,141 @@
+// Userlist data array for filling in info box
+var userListData = [];
+
+// DOM Ready =============================================================
+$(document).ready(function() {
+
+  // Populate the user table on initial page load
+  populateTable();
+  $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+  $('#btnAddUser').on('click', addUser);
+  // How does this work? How does it know which link is clicked?
+  $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+});
+
+// Functions =============================================================
+
+// Fill table with data
+function populateTable() {
+
+  // Empty content string
+  var tableContent = '';
+
+  // jQuery AJAX call for JSON
+  $.getJSON( '/userlist', function( data ) {
+
+    // Stick our user data array into a userlist variable in the global object
+        userListData = data;
+
+    // For each item in our JSON, add a table row and cells to the content string
+    $.each(data, function(){
+      tableContent += '<tr>';
+      tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</td>';
+      tableContent += '<td>' + this.email + '</td>';
+      tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+      tableContent += '</tr>';
+    });
+
+    // Inject the whole content string into our existing HTML table
+    $('#userList table tbody').html(tableContent);
+  });
+};
+
+// Show User Info
+function showUserInfo(event) {
+
+  // Prevent Link from Firing
+  event.preventDefault();
+
+  // Retrieve username from link rel attribute
+  var thisUserName = $(this).attr('rel');
+
+  // Get Index of object based on id value
+  var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+  // Get our User Object
+  var thisUserObject = userListData[arrayPosition];
+
+  //Populate Info Box
+  $('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
+};
+
+// Add User
+function addUser (event) {
+  event.preventDefault();
+
+  var errorCount = 0;
+  $('#addUser input').each(function(index, val) {
+    if($(this).val() === '') {errorCount++;}
+  });
+
+  if(errorCount === 0) {
+    var newUser = {
+      'username': $('#addUser fieldset input#inputUserName').val(),
+      'email': $('#addUser fieldset input#inputUserEmail').val(),
+      'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+      'age': $('#addUser fieldset input#inputUserAge').val(),
+      'location': $('#addUser fieldset input#inputUserLocation').val(),
+      'gender': $('#addUser fieldset input#inputUserGender').val()
+    }
+
+    $.ajax({
+      type: 'POST',
+      data: newUser,
+      url: '/adduser',
+      dataType: 'JSON'
+    }).done(function(response) {
+      if (response.msg === '') {
+        $('#addUser fieldset input').val('');
+        populateTable();
+      } else {
+        alert('Error: ' + response.msg);
+      }
+    });
+  } else {
+    alert('Please fill in all fieldzors!');
+    return false;
+  }
+};
+
+
+// Delete User
+function deleteUser(event) {
+
+  event.preventDefault();
+
+  // Pop up a confirmation dialog
+  var confirmation = confirm('Are you sure you want to delete this user?');
+
+  // Check and make sure the user confirmed
+  if (confirmation === true) {
+
+    // If they did, do our delete
+    $.ajax({
+      type: 'DELETE',
+      url: '/deleteuser/' + $(this).attr('rel')
+    }).done(function( response ) {
+
+      // Check for a successful (blank) response
+      if (response.msg === '') {
+      }
+      else {
+        alert('Error: ' + response.msg);
+      }
+
+      // Update the table
+      populateTable();
+
+    });
+
+  }
+  else {
+
+    // If they said no to the confirm, do nothing
+    return false;
+
+  }
+
+};
